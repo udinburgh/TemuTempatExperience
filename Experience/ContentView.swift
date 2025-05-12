b4654074adeefa7b5b9e83acb870139b6aaa2106
udinburgh <hafizhuddinhanif@gmail.com>
2025-05-12 13:54:54 +0700

    Initial Commit

---
+++ Experience/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Experience
+//
+//  Created by Hafizhuddin Hanif on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Experience/ExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperienceApp.swift
+//  Experience
+//
+//  Created by Hafizhuddin Hanif on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
